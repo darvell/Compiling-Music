26cf61d671a9e141e333df2769df8c471d507407
Darvell Long <contact@darvell.me>
2011-10-24 14:08:37 +1100

    Initial commit.

---
+++ Classes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompilingMusic
+{
+    class Settings
+    {
+        public string bassUser { get; set; }
+        public string bassCode { get; set; }
+        public bool setMode { get; set; }
+        public string setDirectory { get; set; }
+        public string randomCompileDirectory { get; set; }
+        public string randomSuccessDirectory { get; set; }
+        public string randomFailDirectory { get; set; }
+        public bool STFU { get; set; }
+    }
+
+    class Set
+    {
+        public string basePath { get; set; }
+        public string compileSong { get; set; }
+        public string failSong { get; set; }
+        public string successSong { get; set; }
+    }
+
+
+}
